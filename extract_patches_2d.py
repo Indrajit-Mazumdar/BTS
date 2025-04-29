06857ed451c7a48adbb78b538385e1f416a3061c
Indrajit Mazumdar <35587979+Indrajit-Mazumdar@users.noreply.github.com>
2025-04-29 23:01:29 +0530

    Add files via upload

---
+++ extract_patches_2d.py
@@ -0,0 +1,226 @@
+import os
+import sys
+import math
+from glob import glob
+import SimpleITK as sitk
+import random
+import numpy as np
+from tqdm import tqdm
+
+from utils.configuration import config
+from utils.preprocessing import *
+
+
+class PatchExtraction(object):
+    def __init__(self, save_dir):
+        self.save_images_dir = os.path.join(save_dir, "images")
+        if not os.path.isdir(self.save_images_dir):
+            os.makedirs(self.save_images_dir)
+        self.save_masks_dir = os.path.join(save_dir, "masks")
+        if not os.path.isdir(self.save_masks_dir):
+            os.makedirs(self.save_masks_dir)
+        self.extraction_type = config["extraction_type"]
+        self.num_patches = 0
+
+    def extract_patches_cob(self, patient_images):
+        d_image = patient_images.shape[1]
+        h_image = patient_images.shape[2]
+        w_image = patient_images.shape[3]
+        patient_gt = patient_images[4]
+        for slice_idx in range(d_image):
+            arr_lbl = patient_gt[slice_idx, :, :]
+
+            indices_tumor = np.nonzero(arr_lbl)
+
+            num_tumor_elements_img = indices_tumor[0].size
+            if num_tumor_elements_img == 0:
+                continue
+
+            brain_row_center = h_image // 2
+            brain_col_center = w_image // 2
+
+            p_y = (brain_row_center - config["h_patch"] / 2, brain_row_center + config["h_patch"] / 2)
+            p_x = (brain_col_center - config["w_patch"] / 2, brain_col_center + config["w_patch"] / 2)
+            p_y = list(map(int, p_y))
+            p_x = list(map(int, p_x))
+
+            min_row_idx = np.min(indices_tumor[0])
+            max_row_idx = np.max(indices_tumor[0])
+            min_col_idx = np.min(indices_tumor[1])
+            max_col_idx = np.max(indices_tumor[1])
+
+            if min_row_idx < p_y[0]:
+                p_y[0] = min_row_idx
+                p_y[1] = p_y[0] + config["h_patch"]
+            elif max_row_idx >= p_y[1]:
+                p_y[1] = max_row_idx + 1
+                p_y[0] = p_y[1] - config["h_patch"]
+            if min_col_idx < p_x[0]:
+                p_x[0] = min_col_idx
+                p_x[1] = p_x[0] + config["w_patch"]
+            elif max_col_idx >= p_x[1]:
+                p_x[1] = max_col_idx + 1
+                p_x[0] = p_x[1] - config["w_patch"]
+
+            x_patch = patient_images[0:4, slice_idx, p_y[0]:p_y[1], p_x[0]:p_x[1]]
+            y_label = patient_gt[slice_idx, p_y[0]:p_y[1], p_x[0]:p_x[1]]
+
+            num_WT_patch = np.count_nonzero(y_label > 0)
+            if num_WT_patch <= config["2D_tumor_skip_num"]:
+                continue
+
+            x_patch = np.transpose(x_patch, (1, 2, 0)).astype(np.float32)
+
+            y_label[y_label == 4] = 3
+
+            y_label = y_label.reshape(-1)
+
+            y_label = one_hot_encoder(y_label, num_classes=config["num_classes"]).astype(np.uint8)
+            y_label = y_label.reshape(config["h_patch"], config["w_patch"], config["num_classes"])
+
+            np.save(
+                os.path.join(self.save_images_dir, 'x_{}'.format(str(self.num_patches + 1).zfill(7))),
+                x_patch)
+            np.save(
+                os.path.join(self.save_masks_dir, 'y_{}'.format(str(self.num_patches + 1).zfill(7))),
+                y_label)
+            del x_patch, y_label
+
+            self.num_patches += 1
+
+    def extract_patches_random(self, patient_images):
+        patient_patches, patient_labels = [], []
+        patient_gt = patient_images[4]
+        cnt_patient_patches = 0
+        while cnt_patient_patches < config["2D_patches_per_patient"]:
+            slice_idx = np.random.randint(1, config["d_patch"])
+            arr_lbl = patient_gt[slice_idx, :, :]
+            indices_tumor = np.nonzero(arr_lbl)
+            num_elements = indices_tumor[0].size
+            if num_elements <= config["2D_tumor_skip_num"]:
+                continue
+
+            min_tumor_h = np.min(indices_tumor[0])
+            max_tumor_h = np.max(indices_tumor[0])
+            min_tumor_w = np.min(indices_tumor[1])
+            max_tumor_w = np.max(indices_tumor[1])
+
+            patch_h_center = np.random.randint(min_tumor_h, max_tumor_h + 1)
+            patch_w_center = np.random.randint(min_tumor_w, max_tumor_w + 1)
+
+            p_y = (patch_h_center - config["h_patch"] / 2, patch_h_center + config["h_patch"] / 2)
+            p_x = (patch_w_center - config["w_patch"] / 2, patch_w_center + config["w_patch"] / 2)
+            p_y = list(map(int, p_y))
+            p_x = list(map(int, p_x))
+
+            if p_y[0] < 1:
+                p_y[0] = 1
+                p_y[1] = p_y[0] + config["h_patch"]
+            elif p_y[1] > config["h_input"]:
+                p_y[1] = config["h_input"]
+                p_y[0] = p_y[1] - config["h_patch"]
+            if p_x[0] < 1:
+                p_x[0] = 1
+                p_x[1] = p_x[0] + config["w_patch"]
+            elif p_x[1] > config["w_input"]:
+                p_x[1] = config["w_input"]
+                p_x[0] = p_x[1] - config["w_patch"]
+
+            gt = patient_gt[slice_idx, p_y[0]:p_y[1], p_x[0]:p_x[1]]
+            indices_tumor = np.nonzero(gt)
+            num_elements = indices_tumor[0].size
+            if num_elements <= config["2D_tumor_skip_num"]:
+                continue
+
+            img = patient_images[0:4, slice_idx, p_y[0]:p_y[1], p_x[0]:p_x[1]]
+
+            patient_patches.append(img)
+            patient_labels.append(gt)
+            cnt_patient_patches += 1
+
+        x_patches = np.array(patient_patches)
+        y_labels = np.array(patient_labels)
+
+        x_patches = np.transpose(x_patches, (0, 2, 3, 1)).astype(np.float32)
+
+        y_labels[y_labels == 4] = 3
+
+        shp = y_labels.shape[0]
+        y_labels = y_labels.reshape(-1)
+        y_labels = one_hot_encoder(y_labels, num_classes=config["num_classes"]).astype(np.uint8)
+        y_labels = y_labels.reshape(shp, config["h_patch"], config["w_patch"], config["num_classes"])
+
+        shuffle = list(zip(x_patches, y_labels))
+        np.random.shuffle(shuffle)
+        x_patches = np.array([shuffle[i][0] for i in range(len(shuffle))])
+        y_labels = np.array([shuffle[i][1] for i in range(len(shuffle))])
+        del shuffle
+
+        for i in range(cnt_patient_patches):
+            x_patch = x_patches[i]
+            y_label = y_labels[i]
+            np.save(
+                os.path.join(self.save_images_dir, 'x_{}'.format(str(self.num_patches + 1).zfill(5))),
+                x_patch)
+            np.save(
+                os.path.join(self.save_masks_dir, 'y_{}'.format(str(self.num_patches + 1).zfill(5))),
+                y_label)
+            del x_patch, y_label
+            self.num_patches += 1
+        del x_patches, y_labels
+
+    def extract_patches(self, patient_images):
+        if self.extraction_type == "center_of_brain":
+            self.extract_patches_cob(patient_images)
+        elif self.extraction_type == "random_locations":
+            self.extract_patches_random(patient_images)
+
+    def generate_patches(self, paths):
+        num_patients = len(paths)
+
+        for patient_num in tqdm(range(num_patients), desc="Sampling patches"):
+
+            patient_images = read_images(paths[patient_num])
+
+            if config["network_view"] == "Axial":
+                patient_images_ax = patient_images
+                self.extract_patches(patient_images_ax)
+            elif config["network_view"] == "Sagittal":
+                patient_images_sg = np.transpose(patient_images, axes=(0, 3, 1, 2))
+                self.extract_patches(patient_images_sg)
+            elif config["network_view"] == "Coronal":
+                patient_images_cr = np.transpose(patient_images, axes=(0, 2, 1, 3))
+                self.extract_patches(patient_images_cr)
+
+        return self.num_patches
+
+
+def read_images(image_path):
+    t1 = glob(image_path + '/*_t1.nii.gz')
+    t1ce = glob(image_path + '/*_t1ce.nii.gz')
+    t2 = glob(image_path + '/*_t2.nii.gz')
+    flair = glob(image_path + '/*_flair.nii.gz')
+    gt = glob(image_path + '/*_seg.nii.gz')
+    assert ((len(t1) + len(t1ce) + len(t2) + len(flair) + len(gt)) == config["num_mod_gt"]), \
+        "There is a problem here. The problem lies in this patient: {}".format(image_path)
+    scans = [t1[0], t1ce[0], t2[0], flair[0], gt[0]]
+
+    tmp = [sitk.GetArrayFromImage(sitk.ReadImage(scans[k])) for k in range(len(scans))]
+
+    normalized_img = tmp
+    normalized_img[:-1] = normalize(tmp[:-1])
+
+    return np.array(normalized_img)
+
+
+if __name__ == '__main__':
+    paths_train = glob(config["train_brats_path"] + "/*/")
+
+    np.random.shuffle(paths_train)
+
+    save_dir = os.path.join(config["preprocessed_data_dir"], "2D", config["network_view"])
+    if not os.path.isdir(save_dir):
+        os.makedirs(save_dir)
+
+    pipe = PatchExtraction(save_dir=save_dir)
+    num_patches = pipe.generate_patches(paths=paths_train)
---
+++ train_survival.py
@@ -0,0 +1,261 @@
+import random
+import os
+import math
+import sys
+import time
+from collections import OrderedDict
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from functools import partial
+import joblib
+import json
+import matplotlib.pyplot as plt
+from skimage.transform import resize
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+from sklearn.feature_selection import SelectKBest, mutual_info_regression, SelectFromModel
+from sklearn.pipeline import Pipeline
+import xgboost as xgb
+import torch
+
+from utils.configuration import config
+from networks.mtc_net_3d import MTCNet3D
+from utils.preprocessing import *
+from utils.feature_extraction import *
+from utils.feature_selection import *
+from utils.survival_utils import *
+
+activation = {}
+
+
+def get_activation(name):
+    def hook(model, input, output):
+        activation[name] = output.detach()
+
+    return hook
+
+
+def load_pred_model(checkpoint_path, device):
+    model = MTCNet3D(in_channels=config["num_modalities"], out_channels=config["num_classes"])
+
+    model = model.to(device)
+
+    checkpoint = torch.load(checkpoint_path)
+    model.load_state_dict(checkpoint['model_state_dict'])
+
+    model.bottleneck_block.register_forward_hook(get_activation('bottleneck'))
+
+    model.eval()
+
+    return model
+
+
+if __name__ == "__main__":
+    if config["gpu_cpu"] == "GPU" and torch.cuda.is_available():
+        device = 'cuda'
+    else:
+        device = 'cpu'
+
+    surv_train_file_name = 'survival_train.csv'
+    BraTS_ID = 'Brats' + str(config["training_year"])[-2:] + 'ID'
+    surv_col_name = 'Survival_days'
+    resection_col_name = 'Extent_of_Resection'
+
+    model_general_dir = os.path.join(os.getcwd(), 'trained_models', str(config["training_year"]),
+                                     'Survival', config["feature_type"])
+    if not os.path.isdir(model_general_dir):
+        os.makedirs(model_general_dir)
+
+    model_specific_dir = os.path.join(model_general_dir, config["survival_model"])
+    if not os.path.isdir(model_specific_dir):
+        os.makedirs(model_specific_dir)
+
+    training_data_path = config["training_data_path"]
+
+    training_patients_paths = []
+    paths = []
+    for dirpath, dirnames, files in os.walk(training_data_path):
+        training_patients_paths.append(dirpath)
+
+    survival_data = pd.read_csv(os.path.join(training_data_path, surv_train_file_name),
+                                na_filter=False)
+    survival_data = survival_data[survival_data[surv_col_name].astype(str).str.isnumeric()]
+
+    training_features = pd.DataFrame()
+    if config["feature_type"] == "Handcrafted features":
+
+        for index, row in tqdm(survival_data.iterrows(), desc="Feature Extraction", total=survival_data.shape[0]):
+            feature_vector = row.drop(labels=[resection_col_name])
+
+            resection_feature_dict = extract_resection_feature(row[resection_col_name])
+            resection_feature = pd.Series(resection_feature_dict)
+            feature_vector = feature_vector.append(resection_feature)
+
+            t1_image_path, t1ce_image_path, t2_image_path, flair_image_path, seg_mask_path = "", "", "", "", ""
+            for path in training_patients_paths:
+                patient_id = os.path.basename(path)
+                if row[BraTS_ID] == os.path.basename(path):
+                    t1_image_path = os.path.join(path, patient_id + '_t1.nii.gz')
+                    t1ce_image_path = os.path.join(path, patient_id + '_t1ce.nii.gz')
+                    t2_image_path = os.path.join(path, patient_id + '_t2.nii.gz')
+                    flair_image_path = os.path.join(path, patient_id + '_flair.nii.gz')
+                    seg_mask_path = os.path.join(path, patient_id + '_seg.nii.gz')
+                    break
+
+            modalities = OrderedDict()
+            modalities['T1'] = t1_image_path
+            modalities['T1ce'] = t1ce_image_path
+            modalities['T2'] = t2_image_path
+            modalities['FLAIR'] = flair_image_path
+
+            handcrafted_features_dict = extract_handcrafted_features(modalities, seg_mask_path)
+            handcrafted_features = pd.Series(handcrafted_features_dict)
+            feature_vector = feature_vector.append(handcrafted_features)
+
+            training_features = training_features.join(feature_vector)
+
+        training_features.reset_index(drop=True, inplace=True)
+        training_features = training_features.replace(['NA', np.inf], np.nan)
+    elif config["feature_type"] == "Deep features":
+        seg_model_path = os.path.join(os.getcwd(), 'trained_models', str(config["training_year"]),
+                                      'Segmentation', config["seg_model_name"], 'checkpoint',
+                                      'checkpoint_epoch_{:04d}.pth'.format(config["seg_pred_epoch"]))
+
+        seg_model = load_pred_model(seg_model_path, device)
+
+        for index, row in tqdm(survival_data.iterrows(), desc="Feature Extraction", total=survival_data.shape[0]):
+            feature_vector = row.drop(labels=[resection_col_name])
+
+            resection_feature_dict = extract_resection_feature(row[resection_col_name])
+            resection_feature = pd.Series(resection_feature_dict)
+            feature_vector = feature_vector.append(resection_feature)
+
+            t1_image_path, t1ce_image_path, t2_image_path, flair_image_path, seg_mask_path = "", "", "", "", ""
+            for path in training_patients_paths:
+                patient_id = os.path.basename(path)
+                if row[BraTS_ID] == os.path.basename(path):
+                    t1_image_path = os.path.join(path, patient_id + '_t1.nii.gz')
+                    t1ce_image_path = os.path.join(path, patient_id + '_t1ce.nii.gz')
+                    t2_image_path = os.path.join(path, patient_id + '_t2.nii.gz')
+                    flair_image_path = os.path.join(path, patient_id + '_flair.nii.gz')
+                    seg_mask_path = os.path.join(path, patient_id + '_seg.nii.gz')
+                    break
+
+            scans_test = [t1_image_path, t1ce_image_path, t2_image_path, flair_image_path, seg_mask_path]
+
+            test_im = [sitk.GetArrayFromImage(sitk.ReadImage(scans_test[i]))
+                       for i in range(len(scans_test))]
+            test_im = np.array(test_im).astype(np.float32)
+
+            test_image = test_im[0:4]
+
+            test_image = normalize(test_image)
+
+            seg_mask = test_im[-1]
+
+            indices_tumor = np.nonzero(seg_mask)
+
+            min_d_idx = np.min(indices_tumor[0])
+            max_d_idx = np.max(indices_tumor[0])
+            min_h_idx = np.min(indices_tumor[1])
+            max_h_idx = np.max(indices_tumor[1])
+            min_w_idx = np.min(indices_tumor[2])
+            max_w_idx = np.max(indices_tumor[2])
+
+            test_image_cropped = test_image[min_d_idx:max_d_idx + 1, min_h_idx:max_h_idx + 1,
+                                 min_w_idx:max_w_idx + 1, :]
+
+            test_image_resized = resize(test_image_cropped,
+                                        (config["d_resized"], config["h_resized"], config["w_resized"],
+                                         config["num_modalities"]),
+                                        mode='constant',
+                                        preserve_range=True)
+
+            batch_test_image_3d = np.array([test_image_resized])
+
+            batch_test_image_3d = torch.from_numpy(batch_test_image_3d).to(device)
+
+            with torch.no_grad():
+                batch_prediction = seg_model(batch_test_image_3d)
+
+                enc_end_activations = activation['bottleneck'].detach().cpu().numpy()
+
+            enc_end_features = enc_end_activations[0]
+
+            enc_end_flattened_features = enc_end_features.flatten()
+
+            deep_features = pd.Series(enc_end_flattened_features)
+            feature_vector = feature_vector.append(deep_features)
+
+            training_features = training_features.join(feature_vector)
+
+        training_features = training_features.T
+        training_features.reset_index(drop=True, inplace=True)
+        cols = training_features.columns.tolist()
+        training_features.columns = ['DF' + str(x) if isinstance(x, int) else x for x in cols]
+        training_features = training_features.replace(['NA', np.inf], np.nan)
+
+    num_extracted_features = len(
+        training_features.columns) - 2
+
+    x_train = training_features.drop(columns=[BraTS_ID, surv_col_name])
+    y_train = training_features[surv_col_name]
+
+    imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+    x_train = pd.DataFrame(imputer.fit_transform(x_train), columns=x_train.columns)
+
+    joblib.dump(imputer, os.path.join(model_general_dir, config["imputer_name"]))
+
+    scaler = StandardScaler()
+    x_train = pd.DataFrame(scaler.fit_transform(x_train), columns=x_train.columns)
+
+    joblib.dump(scaler, os.path.join(model_general_dir, config["scaler_name"]))
+
+    if config["feature_selection"] == "Pearson correlation coefficient":
+        feature_selector = SelectKBest(score_func=Pearson_correlation_coefficient)
+        feature_selector_scores = SelectKBest(score_func=Pearson_correlation_coefficient, k=num_extracted_features)
+    elif config["feature_selection"] == "Mutual information":
+        feature_selector = SelectKBest(
+            score_func=partial(mutual_info_regression, discrete_features='auto', n_neighbors=3))
+        feature_selector_scores = SelectKBest(
+            score_func=partial(mutual_info_regression, discrete_features='auto', n_neighbors=3),
+            k=num_extracted_features)
+
+    feature_names = pd.DataFrame(x_train.columns.tolist())
+    feature_selector_scores.fit(x_train, y_train)
+    scores = pd.DataFrame(feature_selector_scores.scores_)
+    feature_scores = pd.concat([feature_names, scores], axis=1, ignore_index=True)
+    feature_scores.columns = ['Feature', 'Score']
+    feature_scores = feature_scores.sort_values(by=['Score'], ascending=False)
+
+    params = {'booster': config["xgb_booster"],
+              'disable_default_eval_metric': config["xgb_disable_default_eval_metric"],
+              'eta': config["xgb_eta"],
+              'gamma': config["xgb_gamma"],
+              'max_depth': config["xgb_max_depth"],
+              'min_child_weight': config["xgb_min_child_weight"],
+              'subsample': config["xgb_subsample"],
+              'colsample_bytree': config["xgb_colsample_bytree"],
+              'lambda': config["xgb_lambda"],
+              'objective': config["xgb_objective"]}
+    num_boost_round = config["xgb_num_boost_round"]
+
+    min_MSE = float('inf')
+
+    feature_selector__k = config["num_selected_features"]
+    feature_selector.fit(x_train, y_train)
+    joblib.dump(feature_selector, os.path.join(model_specific_dir, config["feature_selector_name"]))
+    mask = feature_selector.get_support()
+    selected_features = x_train.columns[mask]
+    x_train = pd.DataFrame(feature_selector.transform(x_train), columns=selected_features)
+    feature_names = x_train.columns.tolist()
+    dtrain = xgb.DMatrix(data=x_train.to_numpy(),
+                         label=y_train.to_numpy(),
+                         missing=np.nan,
+                         feature_names=feature_names)
+    model = xgb.train(params=params,
+                      dtrain=dtrain,
+                      num_boost_round=num_boost_round)
+
+    model.save_model(os.path.join(model_specific_dir, config["xgb_model_name"]))
